217569cd65c053a50a7884c42bbe76c83ac82ac8
tashfeenrao <raotashfeen25@gmail.com>
2020-10-25 01:30:02 +0500

    added conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import React, { useState } from "react";
 
 function App() {
   const [num, setNum] = useState(5);
+  const [error, setError] = useState(null);
+
+  const handlePlus = () => {
+    setNum(num + 1);
+    setError(null);
+  };
+  const handleMinus = () => {
+    if (num > 1) {
+      setNum(num - 1);
+    } else {
+      setError("You can't go further :)");
+    }
+  };
   return (
-    <div className="box">
-      <button onClick={() => setNum(num + 1)}>Plus</button>
-      <p>{num}</p>
-      <button onClick={() => setNum(num - 1)}>Minus</button>
+    <div>
+      <div className="box">
+        <button onClick={handlePlus}>Plus</button>
+        <p>{num}</p>
+        <button onClick={handleMinus}>Minus</button>
+      </div>
+      <center>
+        <p>{error}</p>
+      </center>
     </div>
   );
 }
